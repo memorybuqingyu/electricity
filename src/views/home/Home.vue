b70406fb29d5d4bb3bafaee2b06389d61bb9c030
memorybuqingyu <64903770+memorybuqingyu@users.noreply.github.com>
2021-05-27 14:59:18 +0800

    '首页Feature板块'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,33 +1,83 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav">
-      <template v-slot:center>
-        <div>购物街</div>
-      </template>
+      <template v-slot:center><div>购物街</div></template>
     </nav-bar>
-    <swiper>
-      <swiper-item v-for="item in banners" :key="item">
-        <a :href="item.link">
-          <img :src="item.image" alt="" />
-        </a>
-      </swiper-item>
-    </swiper>
+    <home-swiper :banners="banners" />
+
+    <home-recommend-view :recommends="recommends"/>
+
+    <feature-view/>
+
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+      <li>列表21</li>
+      <li>列表22</li>
+      <li>列表23</li>
+      <li>列表24</li>
+      <li>列表25</li>
+      <li>列表26</li>
+      <li>列表27</li>
+      <li>列表28</li>
+      <li>列表29</li>
+      <li>列表30</li>
+      <li>列表31</li>
+      <li>列表32</li>
+      <li>列表33</li>
+      <li>列表34</li>
+      <li>列表35</li>
+      <li>列表36</li>
+      <li>列表37</li>
+      <li>列表38</li>
+      <li>列表39</li>
+      <li>列表40</li>
+      <li>列表41</li>
+      <li>列表42</li>
+      <li>列表43</li>
+      <li>列表44</li>
+      <li>列表45</li>
+      <li>列表46</li>
+      <li>列表47</li>
+      <li>列表48</li>
+      <li>列表49</li>
+      <li>列表50</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/common/navbar/NavBar.vue";
+import HomeSwiper from "./childComps/HomeSwiper.vue";
 import { getHomeMultidata } from "@/network/home.js";
-import { Swiper, SwiperItem } from "@/components/common/swiper/index.js";
+import HomeRecommendView from "./childComps/HomeRecommendView.vue";
+import FeatureView from './childComps/FeatureView.vue';
 
 export default {
   created() {
     //1.请求多个数据
     getHomeMultidata().then((res) => {
-      console.log(res);
       this.banners = res.data.data.banner.list;
-      console.log(this.banners);
-      this.recommends = res.recommends;
+      this.recommends = res.data.data.recommend.list;
+      console.log(this.recommends);
     });
   },
   data() {
@@ -38,18 +88,23 @@ export default {
   },
   components: {
     NavBar,
-    Swiper,
-    SwiperItem,
+    HomeSwiper,
+    HomeRecommendView,
+    FeatureView,
   },
 };
 </script>
 <style>
+#home{
+  padding-top: 44px;
+}
 .home-nav {
   background-color: var(--color-tint);
   color: aliceblue;
-}
-img {
-  width: 100%;
-  height: 100%;
+  position: fixed;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  z-index: 9;
 }
 </style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,34 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in banners" :key="item">
+      <a :href="item.link">
+        <img :src="item.image" alt="" />
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from "@/components/common/swiper/index.js";
+export default {
+  props: {
+    banners: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  components: {
+    Swiper,
+    SwiperItem,
+  },
+};
+</script>
+
+<style>
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
